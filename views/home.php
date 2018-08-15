213b589867637982bdf7e2adbd88d530fa7336be
lance173 <lancesasedor@gmail>
2018-08-15 18:57:16 +0800

    aug-15-18 700hours

--- views/home.php
+++ views/home.php
@@ -184,41 +184,145 @@
                         </div>
                     </div>
                 </div>
-                <div>
-                    <a class="adminca-banner" href="http://admincast.com/adminca/" target="_blank">
-                        <div class="adminca-banner-ribbon"><i class="fa fa-trophy mr-2"></i>PREMIUM TEMPLATE</div>
-                        <div class="wrap-1">
-                            <div class="wrap-2">
-                                <div>
-                                    <img src="../assets/img/adminca-banner/adminca-preview.jpg" style="height:160px;margin-top:50px;" />
-                                </div>
-                                <div class="color-white" style="margin-left:40px;">
-                                    <h1 class="font-bold">ADMINCA</h1>
-                                    <p class="font-16">Save your time, choose the best</p>
-                                    <ul class="list-unstyled">
-                                        <li class="m-b-5"><i class="ti-check m-r-5"></i>High Quality Design</li>
-                                        <li class="m-b-5"><i class="ti-check m-r-5"></i>Fully Customizable and Easy Code</li>
-                                        <li class="m-b-5"><i class="ti-check m-r-5"></i>Bootstrap 4 and Angular 5+</li>
-                                        <li class="m-b-5"><i class="ti-check m-r-5"></i>Best Build Tools: Gulp, SaSS, Pug...</li>
-                                        <li><i class="ti-check m-r-5"></i>More layouts, pages, components</li>
-                                    </ul>
-                                </div>
+            </div>
+            <div class="page-content fade-in-up">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="ibox">
+                            <div class="ibox-head">
+                                <div class="ibox-title"> <i class="fa fa-star-half-alt" aria-hidden="true"></i> Virtual Tour Ratings</div>
                             </div>
-                            <div style="flex:1;">
-                                <div class="d-flex justify-content-end wrap-3">
-                                    <div class="adminca-banner-b m-r-20">
-                                        <img src="../assets/img/adminca-banner/bootstrap.png" style="width:40px;margin-right:10px;" />Bootstrap v4</div>
-                                    <div class="adminca-banner-b m-r-10">
-                                        <img src="../assets/img/adminca-banner/angular.png" style="width:35px;margin-right:10px;" />Angular v5+</div>
-                                </div>
-                                <div class="dev-img">
-                                    <img src="../assets/img/adminca-banner/sprite.png" />
+                            <div class="ibox-body">
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <tbody>
+                                            <tr>
+                                                <td> <img src="../assets/img/user-1.jpg" class="img-ratvw">  </td>
+                                                <td width="350px"> 
+                                                    <div class="ratvw-usrnam">Juan Dela Cruz</div>
+                                                    <div class="font-13">Lorem Ipsum is simply dummy text of the printing and typesetting.</div>
+                                                </td>   
+                                                <td>
+                                                    <div class="td-content"> 
+                                                        <span class="fa fa-star star-checked"></span>
+                                                        <span class="fa fa-star star-checked"></span>
+                                                        <span class="fa fa-star star-checked"></span>
+                                                        <span class="fa fa-star star-checked"></span>
+                                                        <span class="fa fa-star star-checked"></span>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td> <img src="../assets/img/user-2.jpg" class="img-ratvw">  </td>
+                                                <td width="350px"> 
+                                                    <div class="ratvw-usrnam">Maria Clara</div>
+                                                    <div class="font-13">Lorem Ipsum is simply dummy text of the printing and typesetting.</div>
+                                                </td>   
+                                                <td>
+                                                    <div class="td-content"> 
+                                                        <span class="fa fa-star star-checked"></span>
+                                                        <span class="fa fa-star star-checked"></span>
+                                                        <span class="fa fa-star star-checked"></span>
+                                                        <span class="fa fa-star"></span>
+                                                        <span class="fa fa-star"></span>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td> <img src="../assets/img/user-3.jpg" class="img-ratvw">  </td>
+                                                <td width="350px"> 
+                                                    <div class="ratvw-usrnam">Cardo Dalisay</div>
+                                                    <div class="font-13">Lorem Ipsum is simply dummy text of the printing and typesetting.</div>
+                                                </td>   
+                                                <td>
+                                                    <div class="td-content"> 
+                                                        <span class="fa fa-star star-checked"></span>
+                                                        <span class="fa fa-star star-checked"></span>
+                                                        <span class="fa fa-star"></span>
+                                                        <span class="fa fa-star"></span>
+                                                        <span class="fa fa-star"></span>
+                                                    </div>
+                                                </td>
+                                            </tr>                                       
+                                        </tbody>
+                                    </table>
+                                        <center> <a class="btn btn-ratvwall" href="#" role="button"> View All </a> </center>
                                 </div>
                             </div>
                         </div>
-                    </a>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="ibox">
+                            <div class="ibox-head">
+                                <div class="ibox-title"> <i class="fas fa-hourglass"></i> Activity Log</div>
+                                <div>
+                                    <a class="btn btn-actlog btn-sm" href=" :;">View All</a>
+                                </div>
+                            </div>
+                            <div class="ibox-body">
+                                <div class="table-responsive">  
+                                    <table class="table">                                            
+                                        <tbody>
+                                            <tr>
+                                                <td> <div class="td-content"> <i class="fas fa-clone"></i> <b>Michael Jordan</b> added a new Page </div> </td>
+                                            </tr>
+                                            <tr>
+                                                <td> <div class="td-content"> <i class="fas fa-user-plus"></i> <b>Nick Fury</b> added 10 New Student Users </div> </td>
+                                            </tr>
+                                            <tr>
+                                                <td> <div class="td-content"> <i class="fas fa-check"></i> <b>Phil Couson</b> approved an Article </div> </td>                                 
+                                            </tr>
+                                            <tr>
+                                                <td> <div class="td-content"> <i class="fas fa-pen"></i> <b>Carol Denvers</b> edited an Article </div> </td>  
+                                            </tr>
+                                            <tr>
+                                                <td> <div class="td-content"> <i class="fas fa-file-alt"></i> <b>Steve Rogers</b>  wrote an Article </div> </td>
+                                            </tr>                                       
+                                        </tbody>
+                                    </table>                                        
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+            <div>
+                <a class="adminca-banner" href="http://admincast.com/adminca/" target="_blank">
+                    <div class="adminca-banner-ribbon"><i class="fa fa-trophy mr-2"></i>PREMIUM TEMPLATE</div>
+                    <div class="wrap-1">
+                        <div class="wrap-2">
+                            <div>
+                                <img src="../assets/img/adminca-banner/adminca-preview.jpg" style="height:160px;margin-top:50px;" />
+                            </div>
+                            <div class="color-white" style="margin-left:40px;">
+                                <h1 class="font-bold">ADMINCA</h1>
+                                <p class="font-16">Save your time, choose the best</p>
+                                <ul class="list-unstyled">
+                                    <li class="m-b-5"><i class="ti-check m-r-5"></i>High Quality Design</li>
+                                    <li class="m-b-5"><i class="ti-check m-r-5"></i>Fully Customizable and Easy Code</li>
+                                    <li class="m-b-5"><i class="ti-check m-r-5"></i>Bootstrap 4 and Angular 5+</li>
+                                    <li class="m-b-5"><i class="ti-check m-r-5"></i>Best Build Tools: Gulp, SaSS, Pug...</li>
+                                    <li><i class="ti-check m-r-5"></i>More layouts, pages, components</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div style="flex:1;">
+                            <div class="d-flex justify-content-end wrap-3">
+                                <div class="adminca-banner-b m-r-20">
+                                    <img src="../assets/img/adminca-banner/bootstrap.png" style="width:40px;margin-right:10px;" />Bootstrap v4</div>
+                                <div class="adminca-banner-b m-r-10">
+                                    <img src="../assets/img/adminca-banner/angular.png" style="width:35px;margin-right:10px;" />Angular v5+</div>
+                            </div>
+                            <div class="dev-img">
+                                <img src="../assets/img/adminca-banner/sprite.png" />
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+            
             <!-- END PAGE CONTENT-->
 <?php
     require('footer.php');
