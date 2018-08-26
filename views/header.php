eaada42d8d3d6f9bf72dd33afcdd3f56390b8b8a
lancesasedor <lancesasedor@gmail.com>
2018-08-26 22:38:04 +0800

    aug-26-18 2237hours

--- views/header.php
+++ views/header.php
@@ -9,9 +9,11 @@
     <link rel="shortcut icon" type="image/x-icon" href="../assets/img/usclibsystem-transp.png" />
     <!-- GLOBAL MAINLY STYLES-->
     <link href="../assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <!--font  and icon links -->
     <link href="../assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
     <link href="../assets/vendors/themify-icons/css/themify-icons.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
     <!-- PLUGINS STYLES-->
     <!-- THEME STYLES-->
     <link href="../assets/css/main.min.css" rel="stylesheet" />
