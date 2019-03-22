b4929924e15dcd5d9226465f149ff896c24f11ea
lancesasedor <lancesasedor@gmail.com>
2019-03-22 11:31:26 +0800

    3/22/19 11:31

--- views/site-editor.php
+++ views/site-editor.php
@@ -8,17 +8,18 @@
             <!-- START PAGE CONTENT-->
             
               <div class="jumbotron site-editorjumbo" style="">
-        					<h1 class="display-4">WELCOME TO SITE EDITOR</h1>        					
+                <div style="background-color: rgba(0,0,0,0.5); padding: 100px;">
+                  <h1 class="display-4">WELCOME TO SITE EDITOR</h1> 
+                </div>
+        					       					
       				</div>
         </div>  
         <div class="content-wrapper">
-          <div class="page-heading">
-                <h1> WELCOME TO SITE EDITOR </h1>
-            </div>
+          
             
                     
 
-                <!--content-wrapper closing tag will be found in footer.php -->
+        <!--content-wrapper closing tag will be found in footer.php -->
 <?php
     require('footer.php');
 ?>  
